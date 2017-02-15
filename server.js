226b9d194a966ee18d0fc6df528d269b6dd1233f
LordRaymann <Joelraymann@gmail.com>
2017-02-15 20:12:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one will be done and served here as request ASAP');
+    res.sendfile(path.join(__dirname,'ui','article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
